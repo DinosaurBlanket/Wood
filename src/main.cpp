aa3b396fa5024d22db488a75aeecb4fffdc4d526
Jon Arintok <Jon.Arintok@Gmail.com>
2016-08-03 02:52:19 -0400

    prints tokens

--- src/main.cpp
+++ src/main.cpp
@@ -2,22 +2,46 @@
 #include <string>
 #include <iostream>
 
+void handleToken(std::string &t) {
+	std::cout << t << std::endl;
+}
 
 int main(int argc, char** argv ) {
 	if (argc < 2) {
-		puts("usage:\ncscd HelloWorld.cscd");
+		std::cout << "usage:\ncscd HelloWorld.cscd\n";
 		return 1;
 	}
-  char* fileName = argv[1];
+  std::string fileName = argv[1];
 	std::ifstream infile(fileName);
 	if (infile.fail()) {
-		printf("could not open '%s'\n", fileName);
+		std::cout << "could not open '" << fileName << "'\n";
 		return 2;
 	}
 	
-	std::string s;
-	while (infile.peek() != EOF) s.push_back(infile.get());
-	std::cout << s;
+	uint32_t curLine;
+	std::string token;
+	char curFChar = '\0';
+	char prvFChar = '\0';
+	while (infile.peek() != EOF) {
+		prvFChar = curFChar;
+		curFChar = infile.get();
+		switch(curFChar) {
+			case '\n': curLine++; // fall
+			case '\t': // fall
+			case ' ' : // fall
+				switch (prvFChar) {
+					case '\n': break;
+					case '\t': break;
+					case ' ' : break;
+					default:
+						handleToken(token);
+						token.clear();
+				}
+				break;
+			default:
+				token.push_back(curFChar);
+		}
+	}
 	
 	infile.close();
 	return 0;
