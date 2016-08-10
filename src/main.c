45388f5924260bfd2a3f6b3c424c805c2f408378
Jon Arintok <Jon.Arintok@Gmail.com>
2016-08-10 08:39:46 -0400

    lookupToken

--- src/main.c
+++ src/main.c
@@ -6,14 +6,15 @@
 #define numType '.'
 #define narType ':'
 #define parType '|'
-#define anyType '&'
+#define anyType '@'
+#define dubBufChar '~'
 #define cmntStart '('
 #define cmntEnd   ')'
-#define tokSep    '\n'
+#define tokSep    '\n' // token separator
 #define tokSepStr "\n"
-#define rsrvd_
 
-#define _ShouldNotBeHere_ printf("Should Not Be Here: line %i of file%s", __LINE__, __FILE__);
+#define _ShouldNotBeHere_ printf("Should Not Be Here: line %i of %s\n", __LINE__, __FILE__);
+#define fr(i, bound) for (int i = 0; i < (bound); i++)
 
 void printUpTo(const char *toPrint, char upTo) {
 	for (
@@ -38,7 +39,7 @@ typedef enum {
 	ct_par,  // packed array
 	ct_dnar, // number array, double-buffered
 	ct_dpar, // packed array, double-buffered
-	dt_any
+	ct_any
 } cscdType;
 
 typedef union {
@@ -47,7 +48,7 @@ typedef union {
 } anyBox;
 
 typedef struct {
-	char     *name;  // includes parameters, doesn't include "fn "
+	char     *name;  // includes parameters, doesn't include type chars or "fn "
 	uint8_t  *types; // outType first, then params if any
 	uint32_t  paramCount;
 	void     *fn;    // will hold function pointers of many types, or NULL
@@ -58,24 +59,56 @@ double sl_add(double a, double b) {return a + b;}
 double sl_sub(double a, double b) {return a - b;}
 double sl_mul(double a, double b) {return a * b;}
 double sl_div(double a, double b) {return a / b;}
+uint8_t ctp_num[]       = {ct_num};
+// uint8_t ctp_numnum[]    = {ct_num, ct_num};
+uint8_t ctp_numnumnum[] = {ct_num, ct_num, ct_num};
 
-#define cscdStdLibCount 4
+#define cscdStdLibCount 5
 const astNodeDef cscdStdLib[cscdStdLibCount] = {
-	{.name = ".+"tokSepStr".a"tokSepStr".b", .fn = sl_add, .value = 0},
-	{.name = ".-"tokSepStr".a"tokSepStr".b", .fn = sl_sub, .value = 0},
-	{.name = ".*"tokSepStr".a"tokSepStr".b", .fn = sl_mul, .value = 0},
-	{.name = "./"tokSepStr".a"tokSepStr".b", .fn = sl_div, .value = 0}
+	{
+		.name       = "+" tokSepStr "a" tokSepStr "b",
+		.types      = ctp_numnumnum,
+		.paramCount = 2,
+		.fn         = sl_add,
+		.value      = 0
+	},{
+		.name       = "-" tokSepStr "a" tokSepStr "b",
+		.types      = ctp_numnumnum,
+		.paramCount = 2,
+		.fn         = sl_sub,
+		.value      = 0
+	},{
+		.name       = "*" tokSepStr "a" tokSepStr "b",
+		.types      = ctp_numnumnum,
+		.paramCount = 2,
+		.fn         = sl_mul,
+		.value      = 0
+	},{
+		.name       = "/" tokSepStr "a" tokSepStr "b",
+		.types      = ctp_numnumnum,
+		.paramCount = 2,
+		.fn         = sl_div,
+		.value      = 0
+	},{ // this one's just a test
+		.name       = "nine",
+		.types      = ctp_num,
+		.paramCount = 0,
+		.fn         = NULL,
+		.value      = 9
+	}
 };
 
 typedef struct {
-	char           *name; // same as astNodeDef
-	struct astNode *parent;
+	astNodeDef      def;
+	struct astNode *parent;   // in astNodeBuf
 	uint32_t        kidCount; // incremented as parameters are filled
 } astNode;
 
 typedef struct {
 	char    *name;
-	astNode  branch;
+	uint8_t  type;
+	astNode *init;
+	astNode *reev;
 } astRoot;
 
 
@@ -95,38 +128,74 @@ astRootBuf astRoots;
 astNodeBuf astNodes;
 u8Buf      astTypes;
 
-
 typedef enum {
 	ttr_new,
+	ttr_trunk,
 	ttr_branch,
 	ttr_fnParams,
 	ttr_narLit,
 	ttr_parLit
 } thingsToRead;
 
-thingsToRead handleBranchNode(char *token) {
+void lookupToken(char *token, struct astNode *parent, astNode *node) {
+	if (
+		('0' <= *token && *token <= '9')  ||  
+		(*token == '-' && '0' <= token[1] && token[1] <= '9')
+	) {
+		// numlit
+		printUpTo(token, tokSep);
+		puts("  (numlit in lookupToken)");
+		return;
+	}
+	fr (i, cscdStdLibCount) {
+		if (matchUpTo(&cscdStdLib[i].name[0], token, tokSep)) {
+			//match
+			printUpTo(token, tokSep);
+			puts("  (StdLib match in lookupToken)");
+			node->def      = cscdStdLib[i];
+			node->parent   = parent;
+			node->kidCount = 0;
+			return;
+		}
+	}
+	// not found
+	_ShouldNotBeHere_;
+}
+
+thingsToRead handleBranchNode(char *token, astNode *pivot) {
 	printUpTo(token, tokSep);
-	puts("  in handleBranchNode");
+	puts("  (in handleBranchNode)");
 	return ttr_branch;
 }
 
 void handleToken(char *token) {
-	//static astNode *parent = NULL;
+	static astNode     *pivot   = NULL; // latest unfilled node
 	static thingsToRead reading = ttr_new;
 	switch (reading) {
 		case ttr_new:
 			switch (*token) {
 				typeCharCase:
 					pushEmpty_astRootBuf(&astRoots);
-					plast_astRootBuf(astRoots)->name = token;
+					plast_astRootBuf(astRoots)->name = token[1] == dubBufChar ? &token[2] : &token[1];
+					switch (*token) {
+						case numType: plast_astRootBuf(astRoots)->type = ct_num; break;
+						case parType: plast_astRootBuf(astRoots)->type = token[1] == dubBufChar ? ct_dnar : ct_nar; break;
+						case narType: plast_astRootBuf(astRoots)->type = token[1] == dubBufChar ? ct_dpar : ct_par; break;
+						case anyType: plast_astRootBuf(astRoots)->type = ct_any; break;
+					}
 					pushEmpty_astNodeBuf(&astNodes);
-					reading = ttr_branch;
+					plast_astRootBuf(astRoots)->init = pivot = plast_astNodeBuf(astNodes);
+					reading = ttr_trunk;
 					break;
 				default: _ShouldNotBeHere_;
 			}
 			break;
+		case ttr_trunk:
+			lookupToken(token, NULL, pivot);
+			reading = pivot->def.paramCount ? ttr_branch : ttr_new;
+			break;
 		case ttr_branch:
-			reading = handleBranchNode(token);
+			reading = handleBranchNode(token, pivot);
 			break;
 		case ttr_fnParams:
 		case ttr_narLit:
