d6804e11b5e291a27c7dc79541f12907ca47aa0b
Jon Arintok <Jon.Arintok@Gmail.com>
2016-08-11 23:51:05 -0400

    removed root structure

--- src/main.c
+++ src/main.c
@@ -3,11 +3,10 @@
 #include <stdbool.h>
 #include "buf.h"
 
-#define numType '.'
-#define narType ':'
-#define parType '|'
-#define anyType '@'
-#define dubBufChar '~'
+#define numType  '.'
+#define narType  ':'
+#define parType  '|'
+#define thruType '~'
 #define cmntStart '('
 #define cmntEnd   ')'
 #define tokSep    '\n' // token separator
@@ -31,6 +30,15 @@ bool matchUpTo(const char *l, const char *r, char upTo) {
 	if ((!l[i] || l[i] == upTo) && (!r[i] || r[i] == upTo)) return true;
 	return false;
 }
+char *tokenPastType(char *token) {
+	switch (*token) {
+		case numType : // fall
+		case thruType: return token+1;
+		case narType : return token[1] == narType ? token+2 : token+1;
+		case parType : return token[1] == parType ? token+2 : token+1;
+	}
+	return token;
+}
 
 
 typedef enum {
@@ -49,67 +57,39 @@ typedef union {
 
 typedef struct {
 	char     *name;  // includes parameters, doesn't include type chars or "fn "
-	uint8_t  *types; // outType first, then params if any
 	uint32_t  paramCount;
-	void     *fn;    // will hold function pointers of many types, or NULL
-	anyBox    value; // for nodes that output constant, used when fn == NULL
+	void     *fn;    // will hold function pointers of many types
 } astNodeDef;
 
 double sl_add(double a, double b) {return a + b;}
 double sl_sub(double a, double b) {return a - b;}
 double sl_mul(double a, double b) {return a * b;}
 double sl_div(double a, double b) {return a / b;}
-uint8_t ctp_num[]       = {ct_num};
-// uint8_t ctp_numnum[]    = {ct_num, ct_num};
-uint8_t ctp_numnumnum[] = {ct_num, ct_num, ct_num};
+double sl_nine(void) {return 9;}
+void   sl_root(void) {};
+void   sl_numLit(void) {};
 
 #define cscdStdLibCount 5
 const astNodeDef cscdStdLib[cscdStdLibCount] = {
-	{
-		.name       = "+" tokSepStr "a" tokSepStr "b",
-		.types      = ctp_numnumnum,
-		.paramCount = 2,
-		.fn         = sl_add,
-		.value      = 0
-	},{
-		.name       = "-" tokSepStr "a" tokSepStr "b",
-		.types      = ctp_numnumnum,
-		.paramCount = 2,
-		.fn         = sl_sub,
-		.value      = 0
-	},{
-		.name       = "*" tokSepStr "a" tokSepStr "b",
-		.types      = ctp_numnumnum,
-		.paramCount = 2,
-		.fn         = sl_mul,
-		.value      = 0
-	},{
-		.name       = "/" tokSepStr "a" tokSepStr "b",
-		.types      = ctp_numnumnum,
-		.paramCount = 2,
-		.fn         = sl_div,
-		.value      = 0
-	},{ // this one's just a test
-		.name       = "nine",
-		.types      = ctp_num,
-		.paramCount = 0,
-		.fn         = NULL,
-		.value      = 9
-	}
+	{.name = ".+" tokSepStr ".a" tokSepStr ".b", .paramCount = 2, .fn = sl_add},
+	{.name = ".-" tokSepStr ".a" tokSepStr ".b", .paramCount = 2, .fn = sl_sub},
+	{.name = ".*" tokSepStr ".a" tokSepStr ".b", .paramCount = 2, .fn = sl_mul},
+	{.name = "./" tokSepStr ".a" tokSepStr ".b", .paramCount = 2, .fn = sl_div},
+	{.name = ".nine",                            .paramCount = 0, .fn = sl_nine}
 };
 
 typedef struct {
-	astNodeDef      def;
-	struct astNode *parent;   // in astNodeBuf
-	uint32_t        kidCount; // incremented as parameters are filled
+	astNodeDef  def;
+	uint32_t    kidCount; // incremented as parameters are filled
+	anyBox      litVal;
 } astNode;
 
-typedef struct {
-	char    *name;
-	uint8_t  type;
-	astNode *init;
-	astNode *reev;
-} astRoot;
+// typedef struct {
+// 	char    *name;
+// 	uint8_t  type;
+// 	astNode *init;
+// 	astNode *reev;
+// } astRoot;
 
 
 #define SpaceCase case '\n': case '\t': case ' '
@@ -117,89 +97,78 @@ typedef struct {
 case numType:\
 case narType:\
 case parType:\
-case anyType
+case thruType
 
 BufType(char,    charBuf);
-BufType(uint8_t, u8Buf);
 BufType(astNode, astNodeBuf);
-BufType(astRoot, astRootBuf);
 
-astRootBuf astRoots;
 astNodeBuf astNodes;
-u8Buf      astTypes;
+charBuf    astTypes;
 
 typedef enum {
 	ttr_new,
-	ttr_trunk,
 	ttr_branch,
 	ttr_fnParams,
 	ttr_narLit,
-	ttr_parLit
+	ttr_parLit,
+	ttr_error
 } thingsToRead;
 
-void lookupToken(char *token, struct astNode *parent, astNode *node) {
+thingsToRead nodeFromToken(char *token) {
+	printUpTo(token, tokSep);
+	printf("  (in nodeFromToken)\n");
+	pushEmpty_astNodeBuf(&astNodes);
+	astNode *lastNode = plast_astNodeBuf(astNodes);
 	if (
 		('0' <= *token && *token <= '9')  ||  
 		(*token == '-' && '0' <= token[1] && token[1] <= '9')
 	) {
 		// numlit
-		printUpTo(token, tokSep);
-		puts("  (numlit in lookupToken)");
-		return;
+		lastNode->def.name       = token;
+		lastNode->def.paramCount = 0;
+		lastNode->def.fn         = sl_numLit;
+		lastNode->kidCount       = 0;
+		lastNode->litVal.num     = 0;
+		return ttr_branch;
 	}
 	fr (i, cscdStdLibCount) {
-		if (matchUpTo(&cscdStdLib[i].name[0], token, tokSep)) {
+
+		if (matchUpTo(tokenPastType(&cscdStdLib[i].name[0]), token, tokSep)) {
 			//match
-			printUpTo(token, tokSep);
-			puts("  (StdLib match in lookupToken)");
-			node->def      = cscdStdLib[i];
-			node->parent   = parent;
-			node->kidCount = 0;
-			return;
+			lastNode->def        = cscdStdLib[i];
+			lastNode->kidCount   = 0;
+			lastNode->litVal.num = 0;
+			return ttr_branch;
 		}
 	}
 	// not found
 	_ShouldNotBeHere_;
+	return ttr_error;
 }
-
-thingsToRead handleBranchNode(char *token, astNode *pivot) {
+thingsToRead newFromToken(char *token) {
 	printUpTo(token, tokSep);
-	puts("  (in handleBranchNode)");
-	return ttr_branch;
+	printf("  (in newFromToken)\n");
+	astNode *lastNode;
+	switch (*token) {
+		typeCharCase:
+			pushEmpty_astNodeBuf(&astNodes);
+			lastNode = plast_astNodeBuf(astNodes);
+			lastNode->def.name       = token;
+			lastNode->def.paramCount = 1;
+			lastNode->def.fn         = sl_root;
+			lastNode->kidCount       = 0;
+			lastNode->litVal.num     = 0;
+			return ttr_branch;
+		default: _ShouldNotBeHere_;
+	}
+	return ttr_error;
 }
 
 void handleToken(char *token) {
-	static astNode     *pivot   = NULL; // latest unfilled node
 	static thingsToRead reading = ttr_new;
 	switch (reading) {
-		case ttr_new:
-			switch (*token) {
-				typeCharCase:
-					pushEmpty_astRootBuf(&astRoots);
-					plast_astRootBuf(astRoots)->name = token[1] == dubBufChar ? &token[2] : &token[1];
-					switch (*token) {
-						case numType: plast_astRootBuf(astRoots)->type = ct_num; break;
-						case parType: plast_astRootBuf(astRoots)->type = token[1] == dubBufChar ? ct_dnar : ct_nar; break;
-						case narType: plast_astRootBuf(astRoots)->type = token[1] == dubBufChar ? ct_dpar : ct_par; break;
-						case anyType: plast_astRootBuf(astRoots)->type = ct_any; break;
-					}
-					pushEmpty_astNodeBuf(&astNodes);
-					plast_astRootBuf(astRoots)->init = pivot = plast_astNodeBuf(astNodes);
-					reading = ttr_trunk;
-					break;
-				default: _ShouldNotBeHere_;
-			}
-			break;
-		case ttr_trunk:
-			lookupToken(token, NULL, pivot);
-			reading = pivot->def.paramCount ? ttr_branch : ttr_new;
-			break;
-		case ttr_branch:
-			reading = handleBranchNode(token, pivot);
-			break;
-		case ttr_fnParams:
-		case ttr_narLit:
-		case ttr_parLit:
+		case ttr_new:    reading = newFromToken(token); break;
+		case ttr_branch: reading = nodeFromToken(token); break;
 		default: _ShouldNotBeHere_;
 	}
 }
@@ -218,10 +187,9 @@ int main(int argc, char** argv ) {
 	}
 	uint32_t curLine      = 0;
 	uint32_t commentDepth = 0;
-	astRoots = init_astRootBuf(16);
-	astNodes = init_astNodeBuf(16);
-	astTypes = init_u8Buf(16);
-	charBuf tokens = init_charBuf(128);
+	astNodes       = init_astNodeBuf(32);
+	astTypes       = init_charBuf(32);
+	charBuf tokens = init_charBuf(256);
 	push_charBuf(&tokens, tokSep); // to look back on without going OOB
 	char *tokenStart = tokens.data + 1;
 	for (
@@ -250,8 +218,6 @@ int main(int argc, char** argv ) {
 	}
 	if (commentDepth) puts("Warning: unclosed comment\n");
 	fclose(infile);
-	if (matchUpTo(astRoots.data[0].name, ".hyaku", '\n')) puts("success");
-	free(astRoots.data);
 	free(astNodes.data);
 	free(astTypes.data);
 	free(tokens.data);
